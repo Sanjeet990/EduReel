6e837007ff78c4ed03fa08aab57c23b9f8483f60
Sanjeet990 <sanjeet.pathak990@gmail.com>
2026-05-31 16:47:34 +0530

    Updated port

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ echo "Building new image edureel from the root directory..."
 docker build -t edureel .
 
 echo "Starting container edureel detached with envs from backend/.env and mounting uploads directory..."
-docker run -d --name edureel --env-file backend/.env -v "$PWD/uploads:/usr/src/app/uploads" -p 5000:5000 edureel
+docker run -d --name edureel --env-file backend/.env -v "$PWD/uploads:/usr/src/app/uploads" -p 5057:5057 edureel
 
 echo "Deployment complete."
